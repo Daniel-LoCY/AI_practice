cd1313f821c6f23d47067b4623869c72860ed966
ISCOM\cylo <cylo@iscom.com.tw>
2022-05-31 17:42:18 +0800

    Boom!!!

--- diabetes.py
+++ diabetes.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 # from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 # from deepctr_torch.inputs import SparseFeat, DenseFeat, get_feature_names
 # from deepctr_torch.models import *
-# This is a package in preview.
+
 from azureml.opendatasets import Diabetes
 
 diabetes = Diabetes.get_tabular_dataset()
@@ -24,14 +24,14 @@ diabetes_df = diabetes.to_pandas_dataframe() # data
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
     print(diabetes_df)
 # train, test = train_test_split(diabetes_df, test_size=0.2)
-
+# age sex bmi map tc ldl hdl tch ltg glu y
 wide_cols = [
     "AGE",
     "SEX",
     "BMI",
     "BP",
 ]
-crossed_cols = [("AGE", "BMI"), ("AGE", "SEX")]
+crossed_cols = [("AGE", "BMI"), ("AGE", "BP")]
 wide_preprocessor = WidePreprocessor(wide_cols=wide_cols, crossed_cols=crossed_cols)
 X_wide = wide_preprocessor.fit_transform(diabetes_df)
 # print(X_wide)
@@ -61,24 +61,32 @@ tab_mlp = TabMlp(
     continuous_cols=continuous_cols,
 )
 # model = WideDeep(wide=wide, deeptabular=tab_mlp)
-model = torch.load('wd_model.pt/wd_model.pt')
+model = torch.load('wd_model2.pt/wd_model.pt')
 
 trainer = Trainer(model, objective="regression", metrics=[Accuracy])
 trainer.num_workers = 0
-trainer.fit(
-    X_wide=X_wide,
-    X_tab=X_tab,
-    target=target,
-    n_epochs=50000,
-    batch_size=256,
-)
+# trainer.fit(
+#     X_wide=X_wide,
+#     X_tab=X_tab,
+#     target=target,
+#     n_epochs=10000,
+#     batch_size=256,
+# )
 
-trainer.save('wd_model.pt')
-# X_wide = wide_preprocessor.fit_transform(diabetes_df_test)
-# X_tab = tab_preprocessor.fit_transform(diabetes_df_test)
-# pre = trainer.predict(X_wide, X_tab)
-# print(pre)
-# print(diabetes_df_test)
+# trainer.save('wd_model2.pt')
+diabetes_df, diabetes_df_test = train_test_split(diabetes_df, test_size=0.2)
+# for i in diabetes_df_test['Y']:
+#     print(i)
+#     if i not in diabetes_df['Y']:
+#         print("NONONONo")
+
+X_wide = wide_preprocessor.fit_transform(diabetes_df)
+X_tab = tab_preprocessor.fit_transform(diabetes_df)
+pre = trainer.predict(X_wide, X_tab)
+print(pre - list(diabetes_df['Y']) )
+print(diabetes_df['Y'])
+# print(diabetes_df['Y'])
+
 '''
 import numpy as np
 import pandas as pd
