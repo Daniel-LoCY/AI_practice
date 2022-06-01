ff79b5df877c8228dfecdfbca874948abd3c9bbe
ISCOM\cylo <cylo@iscom.com.tw>
2022-06-01 17:03:16 +0800

    add model3

--- diabetes.py
+++ diabetes.py
@@ -7,23 +7,15 @@ from pytorch_widedeep import Trainer
 from pytorch_widedeep.metrics import Accuracy
 import pandas as pd
 import torch
-# from sklearn.metrics import log_loss, roc_auc_score
 from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-# from deepctr_torch.inputs import SparseFeat, DenseFeat, get_feature_names
-# from deepctr_torch.models import *
-
 from azureml.opendatasets import Diabetes
 
 diabetes = Diabetes.get_tabular_dataset()
 diabetes_df = diabetes.to_pandas_dataframe() # data
 diabetes_df, diabetes_df_test = train_test_split(diabetes_df, test_size=0.2)
-diabetes_df = diabetes.to_pandas_dataframe() # data
 
-# print(diabetes_df)
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
     print(diabetes_df)
-# train, test = train_test_split(diabetes_df, test_size=0.2)
 # age sex bmi map tc ldl hdl tch ltg glu y
 wide_cols = [
     "AGE",
@@ -34,7 +26,6 @@ wide_cols = [
 crossed_cols = [("AGE", "BMI"), ("AGE", "BP")]
 wide_preprocessor = WidePreprocessor(wide_cols=wide_cols, crossed_cols=crossed_cols)
 X_wide = wide_preprocessor.fit_transform(diabetes_df)
-# print(X_wide)
 
 cat_embed_cols = [
     ("AGE"),
@@ -45,14 +36,11 @@ cat_embed_cols = [
 continuous_cols = ["S1", "S2", "S3", "S4", "S5", "S6"]
 target = "Y"
 target = diabetes_df[target].values
-# print(target)
 
 tab_preprocessor = TabPreprocessor(
     cat_embed_cols=cat_embed_cols, continuous_cols=continuous_cols
 )
 X_tab = tab_preprocessor.fit_transform(diabetes_df)
-
-# print(X_tab)
 
 wide = Wide(input_dim=np.unique(X_wide).shape[0], pred_dim=1)
 tab_mlp = TabMlp(
@@ -60,7 +48,7 @@ tab_mlp = TabMlp(
     cat_embed_input=tab_preprocessor.cat_embed_input,
     continuous_cols=continuous_cols,
 )
-# model = WideDeep(wide=wide, deeptabular=tab_mlp)
+model = WideDeep(wide=wide, deeptabular=tab_mlp)
 model = torch.load('wd_model2.pt/wd_model.pt')
 
 trainer = Trainer(model, objective="regression", metrics=[Accuracy])
@@ -69,23 +57,18 @@ trainer.num_workers = 0
 #     X_wide=X_wide,
 #     X_tab=X_tab,
 #     target=target,
-#     n_epochs=10000,
+#     n_epochs=300000,
 #     batch_size=256,
 # )
-
 # trainer.save('wd_model2.pt')
-diabetes_df, diabetes_df_test = train_test_split(diabetes_df, test_size=0.2)
-# for i in diabetes_df_test['Y']:
-#     print(i)
-#     if i not in diabetes_df['Y']:
-#         print("NONONONo")
 
 X_wide = wide_preprocessor.fit_transform(diabetes_df)
 X_tab = tab_preprocessor.fit_transform(diabetes_df)
-pre = trainer.predict(X_wide, X_tab)
-print(pre - list(diabetes_df['Y']) )
+pre = trainer.predict(X_wide,X_tab)
+print(pre)
 print(diabetes_df['Y'])
-# print(diabetes_df['Y'])
+print(pre - list(diabetes_df['Y']) )
+
 
 '''
 import numpy as np
